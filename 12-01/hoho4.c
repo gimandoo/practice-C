f21d86f28ca1d795f50cb1eae18be7daeb1fb006
gimandoo <alswn7109@gmail.com>
2024-12-01 10:24:00 +0000

    he

---
+++ 12-01/hoho4.c
@@ -0,0 +1,14 @@
+// 전치(--a)와 후치(a++)
+
+#include <stdio.h>
+
+int mian(){
+    int a = 10;
+    int b;
+
+    printf("===후치 증가 연산===\n");
+    a++;
+    b = a;
+    pirntf(" a : %d\n", a};
+    printf(" b : %d\n", b);
+    }
